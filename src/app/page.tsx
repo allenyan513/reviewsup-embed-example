449565d5a36ba8611e9c9a8af0b6271c0649bdf7
alin <wsyanligang@gmail.com>
2025-07-04 16:21:01 -0300

    upgrade

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
-import {ReviewsUpWrapper} from "@/components/reviewsup-wrapper";
+'use client'
+import {ShowcaseClient} from "@reviewsup/embed-react";
 
-export default async function Home() {
+const showcaseId = [
+  '2c337712ccd',
+  '92620b6dda4',
+  'b68f3f0a0a2'
+]
+
+export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-100">
-      <ReviewsUpWrapper/>
+    <div className="flex flex-col w-full items-center justify-center gap-16 p-4 bg-white">
+      {showcaseId.map((id) => (
+        <ShowcaseClient
+          key={id}
+          showcaseId={id}
+        />
+      ))}
     </div>
   );
 }
